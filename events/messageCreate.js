57e7d148773199759359e49a049d0fc8226ac5e7
abde-prefetch <helixoi131@gmail.com>
2026-07-21 20:50:47 +0200

    debug: add messageCreate log to inspect runtime traffic

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -13,6 +13,8 @@ module.exports = {
   async execute(message, client) {
     if (message.author.bot || !message.guild) return;
 
+    console.log(`[DEBUG] Bot Gestion - Message reçu de ${message.author.tag} (${message.author.id}) : "${message.content}"`);
+
     const guildId = message.guild.id;
     const config = client.db.getGuildConfig(guildId);
     const globalData = client.db.getGlobalData();
